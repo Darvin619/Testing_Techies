29d063d54469a3f06266860b717a971cbaf31de5
vanga <vanga@LAPTOP-J4T9LKK5>
2023-09-19 21:14:03 +0530

    Second Commit

--- src/day12/Array2.java
+++ src/day12/Array2.java
@@ -6,6 +6,8 @@ public class Array2 {
 		int a[] = {10,20,70,5};
 		int b[] = {25,15,20,45};
 		
+		int c[] = new int [a.length];
+		
 		int sum =a[0];
 		int i =0;
 		
@@ -20,11 +22,21 @@ public class Array2 {
 //	} System.out.println(sum);
 //		}}
 
-	int largest = a[0];
-	for (i=0; i<a.length; i++) {
-		if(largest<a[i]) {
-			largest=a[i];
+//	int largest = a[0];
+//	for (i=0; i<a.length; i++) {
+//		if(largest<a[i]) {
+//			largest=a[i];
+//		}
+//	}System.out.println(largest);
+		
+		if(a.length != b.length) {
+			System.out.println("Length should be same for summation");
 		}
-	}System.out.println(largest);
+		for (i=0; i<c.length; i++) {
+			
+			c[i]= a[i]+b[i];
+			System.out.print(c[i] + " ");
+		}
+		
 	}}
 	
